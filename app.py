557b28d429787161083a659f9d34b8986b4297aa
Muhammad-Davaa <mdava@students.amikom.ac.id>
2023-07-06 01:42:13 +0700

    main_baru

--- app.py
+++ app.py
@@ -1,40 +1,44 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, validators
+from wtforms.validators import DataRequired, Email, ValidationError
+from werkzeug.security import check_password_hash
+# from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'anak-sholih'
+
+class RegistrationForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired(), Email()])
+    password1 = PasswordField('password1', validators=[validators.DataRequired(), validators.Length(min=3), validators.EqualTo('password2', message='Passwords do not match')])
+    password2 = PasswordField('password2', validators=[validators.DataRequired(), validators.EqualTo('password1', message='Passwords do not match')])
+    submit = SubmitField('register')
+
+    # def validate_email(self, field):
+    #     if check_email_exists(field.data):
+    #         raise ValidationError('This email has already been registered!')
 
 @app.route("/")
-def halaman_home():
-    css_url = url_for('static', filename='css/style.css');
-    return render_template("home.html", css_url=css_url);
+def Home():
+    title = 'Halaman Home'
+    return render_template('sbadmin2/index.html', title=title)
 
+        
 @app.route("/login")
-def halaman_login():
-    return render_template("login.html");
+def Login():
+    css_url = url_for('static', filename='css/sb-admin-2.min.css');
+    return render_template('sbadmin2/login.html', css_url=css_url)
 
-@app.route("/registrasi")
-def halaman_registrasi():
-    return render_template("registrasi.html");
-
-@app.route("/member")
-def data_member():
-    return render_template("member.html");
-
-@app.route("/transaksi_member")
-def data_transaksi_memer():
-    return render_template("transaksi_member.html");
-
-@app.route("/transaksi_umum")
-def data_transaksi_umum():
-    return render_template("transaksi_umum.html");
-
-@app.route("/cetak_laporan")
-def cetak_laporan():
-    return render_template("cetak_laporan.html");
-
-@app.route("/contac")
-def hub_contact():
-    return render_template("contact.html");
-
+@app.route("/register", methods=['GET', 'POST'])
+def Register():
+    title = 'Halaman Registrasi'
+    form = RegistrationForm()
+    if form.validate_on_submit() == False:
+        return render_template('sbadmin2/register.html', title=title, form=form)
+    else:
+        return "<p>Berhasil !</p>"
 
 if __name__ == "__main__":
     app.run(debug=True)
